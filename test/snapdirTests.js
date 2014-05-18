34f07b6f6a957e658aede29e0abc8c1f3827d626
Jason Jarrett <jason@elegantcode.com>
2014-05-17 22:21:26 -0700

    adding no-sha option. refactoring input

--- test/snapdirTests.js
+++ test/snapdirTests.js
@@ -10,14 +10,28 @@ var sut = require('../lib/snapdir.js');
 describe('snapdir', function(){
 
     it('#getTree()', function(done){
-        sut.getTree("sampleDir", function(err, result){
+        sut.getTree({
+            globPattern: "sampleDir"
+        }, function (err, result) {
             this.verifyAsJSON(result);
             done();
         }.bind(this));
     });
 
     it('#getTreeSync()', function(){
-        var result = sut.getTreeSync("sampleDir");
+        var result = sut.getTreeSync({
+            globPattern: "sampleDir"
+        });
         this.verifyAsJSON(result);
     });
+
+
+    it('#getTreeSync() - noSha', function () {
+        var result = sut.getTreeSync({
+            globPattern: "sampleDir",
+            noSha: true
+        });
+        this.verifyAsJSON(result);
+    });
+
 });
